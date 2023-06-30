8d72a56ff8f224e4213ff644db5ccaca072165dd
thisispriyankpatel <priyank.patel5@dcmail.ca>
2023-06-30 17:50:46 -0400

    New Update For 'User Specific Data'

--- Base/views.py
+++ Base/views.py
@@ -1,3 +1,6 @@
+from typing import Any, Dict
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.list  import ListView
 from django.views.generic.detail  import DetailView
@@ -25,6 +28,16 @@ class TaskList(LoginRequiredMixin, ListView):
     model = Task 
     context_object_name = 'tasks'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete=False).count()        
+        return context
+
+
+
+
+
 class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'task'
@@ -32,12 +45,23 @@ class TaskDetail(LoginRequiredMixin, DetailView):
 
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description','complete']
     success_url = reverse_lazy('tasks')
+
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate,self).form_valid(form)
+
+
+
+
+
+
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description','complete']
     success_url = reverse_lazy('tasks')
 
 class DeleteView(LoginRequiredMixin, DeleteView):
