dffe081f9c34252721531a4f1ac91e9d70f24b30
thisispriyankpatel <priyank.patel5@dcmail.ca>
2023-06-30 18:34:49 -0400

    User Registration Feature Added

--- Base/views.py
+++ Base/views.py
@@ -1,15 +1,17 @@
 from typing import Any, Dict
 from django.forms.models import BaseModelForm
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list  import ListView
 from django.views.generic.detail  import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 
 from django.contrib.auth.views import LoginView
 
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 from .models import Task
 
@@ -22,6 +24,22 @@ class CustomLoginView(LoginView):
     def get_success_url(self):
         return reverse_lazy('tasks')
 
+class RegisterPage(FormView):
+    template_name = 'Base/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('task') 
+        return super(RegisterPage,self).get(*args, **kwargs)
 
 
 class TaskList(LoginRequiredMixin, ListView):
