635bb963100b9b834713642816eb41ed1b97e208
thisispriyankpatel <priyank.patel5@dcmail.ca>
2023-07-04 16:18:51 -0400

    Deploy Changes

--- ToDo_List/settings.py
+++ ToDo_List/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=p0(vlv_c8vbzf@=lga_881z*4w@!w3hh^silt%i^!n1p2i%=e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
