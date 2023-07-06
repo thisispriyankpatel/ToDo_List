6435b8bd43e8cb7bd7e0f21cc7c35cad9c595681
thisispriyankpatel <priyank.patel5@dcmail.ca>
2023-07-05 21:44:45 -0400

    Error issue

--- ToDo_List/urls.py
+++ ToDo_List/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.views.static import serve
-from django.conf.urls import url
+from django.urls import re_path
 from django.conf import settings
 
 
@@ -25,6 +25,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('Base.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
